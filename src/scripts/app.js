ca2f29d0851d5e9950111cc1a0ef220f99d75c9b
mei-riko <dobrovolska.marina@gmail.com>
2022-10-28 18:44:04 +0800

    1st commit

---
+++ src/scripts/app.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+$(function() {
+    // Animate Scroll
+    $(document).on('click', '[data-trigger="scroll"]', function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+    // Link Disable
+    $(document).on('click', '[data-trigger="click"]', function(e){
+        e.preventDefault();
+    })
+    // Fancybox
+    Fancybox.bind("[data-fancybox]", {
+        autoFocus: false,
+        dragToClose: false,
+    });
+});
+
+import './mouseup';
+import '../items/input/input';
---
+++ src/scripts/mouseup.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+// import {navbarMobileClose} from '../items/slider/slider';
+
+$(document).on('mouseup', function (e){
+    // let navbarActive = $('.nav.nav--open');
+    // let toggleActive = $('.header__toggle.header__toggle--open');
+    
+    // //   && navbarActive.has(e.target).length === 0 // и не по его дочерним 
+    // if (!navbarActive.is(e.target) // клик был не по блоку
+    //       && !toggleActive.is(e.target)
+    // ) {
+    //     $('body').removeClass('show-overlay');
+    //     toggleActive.removeClass('header__toggle--open');
+    //     navbarActive.removeClass('nav--open');
+    //     navbarActive.slideUp();
+    // }
+});
